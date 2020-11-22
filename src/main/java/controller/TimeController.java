6dc0528dedf83f785d7bbbe8cd17a6f9a7d9e103
thanhdt246 <dothanh24062000@gmail.com>
2020-11-22 12:12:32 +0700

    upload exercise

---
+++ src/main/java/controller/TimeController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+
+    @GetMapping("/timezone")
+    public ModelAndView getTimeByTimeZone(Model model, @RequestParam(name="city",required = false,defaultValue = "Asia/Ho_Chi_Minh") String city){
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone locale = TimeZone.getTimeZone(city);
+        long locale_time = date.getTime() + (locale.getRawOffset() - local.getRawOffset());
+        date.setTime(locale_time);
+        model.addAttribute("city",city);
+        model.addAttribute("date",date);
+        return new ModelAndView("timezone");
+    }
+}
